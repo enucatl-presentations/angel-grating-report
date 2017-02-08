9bc937cf6f27bfea4d8fccdcd203460415360e20
Matteo Abis <gmatteo..abis@gmail.com>
2017-02-08 17:49:58 +0100

    Automated commit at 2017-02-08 16:49:58 UTC by middleman-deploy 2.0.0-alpha

---
+++ images/save_fig.py
@@ -0,0 +1,43 @@
+import h5py
+import numpy as np
+from scipy import stats
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import click
+
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.option("--big_crop", nargs=4, type=int, default=[0, -1, 0, -1])
+@click.option("--dataset_name")
+@click.option("--output")
+def main(filename, dataset_name, big_crop, output):
+    input_file = h5py.File(filename, "r")
+    dataset = input_file[dataset_name]
+    min_x, max_x, min_y, max_y = big_crop
+    print("original shape", dataset.shape)
+    dataset = dataset[min_y:max_y, min_x:max_x, ...]
+    if dataset.ndim > 2:
+        dataset = dataset[..., 0]
+    if dataset_name == "postprocessing/visibility":
+        median_visibility = np.median(dataset)
+        print("median_vis = ", median_visibility)
+    plt.figure()
+    limits = stats.mstats.mquantiles(
+        dataset,
+        prob=[0.02, 0.98])
+    print(limits)
+    image = plt.imshow(
+        dataset,
+        aspect='auto',
+        clim=limits)
+    plt.colorbar()
+    plt.savefig(
+        output,
+        bbox_inches="tight",
+        dpi=120)
+
+
+
+if __name__ == "__main__":
+    main()
